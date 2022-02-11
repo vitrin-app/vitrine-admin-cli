2c4218083f01d18c0fb94059f73980c7b0c4b654
loreanvictor <ghanizadeh.eugene@gmail.com>
2022-02-11 20:04:39 +0100

    add admin URLs to about

--- src/about.tsx
+++ src/about.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Text } from 'ink'
 
-import { Title, Line } from './util'
+import { Title, Line, Divider, Field } from './util'
+import { theme } from './theme'
+import { BASE_URL as BASE_URL_LISTINGS } from './api/listings'
+import { BASE_URL as BASE_URL_VIDEOS } from './api/videos'
+import { BASE_URL as BASE_URL_AUTH } from './api/auth'
 
 
 export const About = () => {
@@ -10,6 +14,16 @@ export const About = () => {
       <Title>❓ About this CLI</Title>
       <Line><Text>Yeah there was no real reason to do this honestly.</Text></Line>
       <Line><Text>However, I enjoyed building it, and I think thats what counts.</Text></Line>
+      <Divider/>
+      <Field label='Listings URL'>
+        <Text color={theme.primary}>{BASE_URL_LISTINGS}</Text>
+      </Field>
+      <Field label='Videos URL'>
+        <Text color={theme.primary}>{BASE_URL_VIDEOS}</Text>
+      </Field>
+      <Field label='Auth URL'>
+        <Text color={theme.primary}>{BASE_URL_AUTH}</Text>
+      </Field>
     </>
   )
 }
--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import { get, post, del } from './base'
 
 
-const BASE_URL = process.env['API_AUTH_URL'] || 'https://auth.vitrin.cloud'
+export const BASE_URL = process.env['API_AUTH_URL'] || 'https://auth.vitrin.cloud'
 
 
 export const verifyEmail = async (email: string) => {
